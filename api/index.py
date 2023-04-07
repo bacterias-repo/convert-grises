83b86d35fec3bdbb3f6f5b46d62063c04c0f2159
bacterias-repo <126705858+bacterias-repo@users.noreply.github.com>
2023-04-07 10:03:36 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,36 +1,33 @@
-import os
+from fastapi import FastAPI, File, UploadFile
 import cv2
-import requests
-from github import Github
+import numpy as np
+import os
+import urllib.request
 
-def convert_to_gray(image_path):
-    image = cv2.imread(image_path)
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return gray_image
+app = FastAPI()
 
-def upload_to_github(image_path, repo_name, repo_branch, github_token):
-    g = Github(github_token)
-    user = g.get_user()
-    repo = user.get_repo(repo_name)
-    branch = repo.get_branch(repo_branch)
-    file_path = os.path.basename(image_path)
-    with open(image_path, 'rb') as file:
-        content = file.read()
-    repo.create_file(file_path, f'Added {file_path}', content, branch=branch.name)
-    return f'Image uploaded to {repo_name} repository in {repo_branch} branch.'
-
-def convert_to_grayscale(event):
-    image_url = event['body-json']['image_url']
-    github_token = event['headers']['x-github-token']
-    repo_name = event['headers']['x-github-repo']
-    repo_branch = event['headers']['x-github-branch']
-    image_filename = os.path.basename(image_url)
-    image_response = requests.get(image_url)
-    image_path = f'/tmp/{image_filename}'
-    with open(image_path, 'wb') as file:
-        file.write(image_response.content)
-    gray_image = convert_to_gray(image_path)
-    cv2.imwrite(image_path, gray_image)
-    upload_to_github(image_path, repo_name, repo_branch, github_token)
-    return f'Image {image_filename} converted to grayscale and uploaded to GitHub repository.'
-
+@app.post("/convert")
+async def convert_image(file: UploadFile = File(...)):
+    # Leer la imagen subida y guardarla en una carpeta temporal
+    with open(file.filename, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    # Cargar la imagen con la librería cv2
+    img = cv2.imread(file.filename)
+    
+    # Convertir la imagen a escala de grises
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Guardar la imagen en el repositorio de GitHub
+    img_name = os.path.splitext(file.filename)[0]
+    cv2.imwrite(f"{img_name}_gray.jpg", gray)
+    repo_url = "https://github.com/usuario/repositorio.git"
+    repo_dir = "/ruta/a/repositorio/"
+    os.chdir(repo_dir)
+    os.system("git pull")
+    os.system("git add .")
+    os.system(f'git commit -m "Agregada imagen en escala de grises {img_name}"')
+    os.system("git push")
+    urllib.request.urlopen(f"https://api.github.com/repos/usuario/repositorio/actions/workflows/workflow.yml/dispatches",
+                            data=b'{"ref":"main"}')
+    return {"message": f"Imagen convertida a escala de grises y guardada en {repo_url}"}
